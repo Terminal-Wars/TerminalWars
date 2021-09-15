b51faa3e14c4aa2a032e7d453b35be98b5d0451e
IoIxD <alphaproject217@gmail.com>
2021-09-15 15:58:14 -0700

    ¯\_(ツ)_/¯

---
+++ static/url.js
@@ -0,0 +1,4 @@
+// either one of these needs to be set. As far as I'm concerned, one can't be set automatically. if the other doesn't work.
+// after commenting out the correct variable, you will need to copy or rename this to url.js
+//export const socket = new WebSocket('ws://localhost:2191/socket');
+//export const socket = new WebSocket('wss://ipv4.ioi-xd.net:2191/socket');
