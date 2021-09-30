1997ded5c945b676b999f289bdda933538e0c0ec
IoIxD <alphaproject217@gmail.com>
2021-09-30 12:24:32 -0700

    fix to ping code

--- static/js/ping.js
+++ static/js/ping.js
@@ -2,11 +2,11 @@ export let ping = undefined;
 export function pingSite() {
   let start = new Date().getTime();
   let http = new XMLHttpRequest();
-  http.open("GET", window.location);
+  http.open("GET", window.location.hostname);
   http.onreadystatechange = function() {
     if(http.readyState == 4) {
       let end = new Date().getTime(); 
-      ping = (end-start)+20;
+      ping = (end-start)+10;
     }
   }
   try {
