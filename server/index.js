7544b522aaebe08f45b2d5a9a99ca61368cc39b7
IoIxD <alphaproject217@gmail.com>
2021-12-21 09:17:37 -0700

    basic web server finished

--- server/index.js
+++ server/index.js
@@ -7,16 +7,22 @@ const mime = require('mime');
 		switch(req.url) {
 			case "/": 				fileName = "./templates/game.html"; break;
 			case "/favicon.ico": 	fileName = "./static/gfx/logo.ico"; break;
+			case "/socket": 		break; // todo: socket server
 			default: 				fileName = "."+req.url; break;
 		}
-		let headerType = `'Content-Type': ${mime.getType(fileName)}`;
-		res.writeHead(200, {headerType});
-		const file = fs.createReadStream(fileName);
-		file.on('data', (chunk) => {
-			res.write(chunk);
-		})
-		file.on('end', () => {
-			res.end();
+		fs.access(fileName, (err) => {
+			if(err) {
+				res.writeHead(404);
+			} else {
+				res.writeHead(200, {'Content-Type': mime.getType(fileName)});
+				const file = fs.createReadStream(fileName);
+				file.on('data', (chunk) => {
+					res.write(chunk);
+				})
+				file.on('end', () => {
+					res.end();
+				});
+			}
 		});
 	}).listen(2191);
 })();
