6d4f7d6b4571fba03cb292168fd2c484389e4021
IoIxD <alphaproject217@gmail.com>
2021-12-22 10:32:18 -0700

    fuck this i'm rewriting the server in node.js myself

--- server/index.js
+++ server/index.js
@@ -1,28 +1,7 @@
-const http = require('http');
-const fs = require('fs');
-const mime = require('mime');
-(async () => {
-	http.createServer(function(req, res) {
-		let fileName;
-		switch(req.url) {
-			case "/": 				fileName = "./templates/game.html"; break;
-			case "/favicon.ico": 	fileName = "./static/gfx/logo.ico"; break;
-			case "/socket": 		break; // todo: socket server
-			default: 				fileName = "."+req.url; break;
-		}
-		fs.access(fileName, (err) => {
-			if(err) {
-				res.writeHead(404);
-			} else {
-				res.writeHead(200, {'Content-Type': mime.getType(fileName)});
-				const file = fs.createReadStream(fileName);
-				file.on('data', (chunk) => {
-					res.write(chunk);
-				})
-				file.on('end', () => {
-					res.end();
-				});
-			}
-		});
-	}).listen(2191);
-})();
+const http = require('./core/http.js');
+const ws = require('./core/http.js');
+
+// HTTP server
+http.httpFunc();
+
+// Websocket server
